001e7565d70ce78067541370b7ce2e46d83d8e01
Shubham Rawat <46570973+shubhamrawat140798@users.noreply.github.com>
2021-05-09 20:16:01 +0530

    Initial Commit
    return the sum of number( using decrement operation on the variable)

---
+++ Recursion/Sum.java
@@ -0,0 +1,15 @@
+public class recursion{
+	public static void main(String[] args){
+		int result = sum(10);
+		System.out.println(result);
+	}
+	public static int sum(int k){
+		if (k>0){
+			System.out.print(k+ " -> ");
+			return k + sum(k - 1);
+		}
+		else{
+			return 0;
+		}
+}
+}
